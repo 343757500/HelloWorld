44791b564439404b62fc4212f6c27d9a75ff6af9
20170426-094954\Administrator <343757500@qq.com>
2017-08-16 15:57:53 +0800

    第三方微信支付没法完成

--- app/src/main/java/com/micro/hellowold/MainActivity.java
+++ app/src/main/java/com/micro/hellowold/MainActivity.java
@@ -13,15 +13,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.micro.hellowold.interfaces.ILoginCallBack;
+import com.micro.hellowold.tools.LoginHttpModule;
+import com.micro.hellowold.tools.LoginManager;
+import com.micro.hellowold.wxapi.WXPayEntryActivity;
 import com.tencent.connect.UserInfo;
-import com.tencent.connect.common.Constants;
+
 import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.Tencent;
-import com.tencent.tauth.UiError;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.xutils.x;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,13 +38,10 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.bt_pay)
     Button btPay;
 
-    private boolean isServerSideLogin = false;
+    public static final String APP_ID = "wx690b9cf56a4ec056";
 
     public static Tencent mTencent;
-    private static String token;
-    private static String expires;
-    private static String openId;
-
+    private IUiListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +55,24 @@ public class MainActivity extends AppCompatActivity {
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginQQ();
+                //loginQQ();
+                LoginManager loginManager=new LoginManager();
+                loginManager.loginThird(MainActivity.this, LoginHttpModule.LoginThirdType.QQ, mTencent, new ILoginCallBack() {
+                    @Override
+                    public void onLoginSuccess(String msg,IUiListener iUiListener) {
+                        Toast.makeText(getApplicationContext(), "111", Toast.LENGTH_SHORT).show();
+                        listener=iUiListener;
+                        tvReturnmessage.setText(msg);
+                    }
+
+                    @Override
+                    public void onLoginFailed(String msg,IUiListener iUiListener) {
+                        Toast.makeText(getApplicationContext(), "222", Toast.LENGTH_SHORT).show();
+                        listener=iUiListener;
+                        tvReturnmessage.setText(msg);
+
+                    }
+                });
 
             }
         });
@@ -66,155 +80,12 @@ public class MainActivity extends AppCompatActivity {
         btPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             // "111111"
+                Intent intent=new Intent(MainActivity.this,WXPayEntryActivity.class);
+                startActivity(intent);
             }
         });
 
     }
-
-    /**
-     * ------------------------QQ第三方登录--------------------
-     */
-    public void loginQQ() {
-        /** 判断是否登陆过 */
-        if (!mTencent.isSessionValid()) {
-            mTencent.login(this, "all", loginListener);
-        }/** 登陆过注销之后在登录 */
-        else {
-            mTencent.logout(this);
-            mTencent.login(this, "all", loginListener);
-        }
-    }
-
-    IUiListener loginListener = new BaseUiListener() {
-        @Override
-        protected void doComplete(JSONObject values) {
-            initOpenidAndToken(values);
-            updateUserInfo();
-        }
-    };
-
-    /**
-     * QQ登录第二步：存储token和openid
-     */
-    public static void initOpenidAndToken(JSONObject jsonObject) {
-        try {
-            token = jsonObject.getString(Constants.PARAM_ACCESS_TOKEN);
-            expires = jsonObject.getString(Constants.PARAM_EXPIRES_IN);
-            openId = jsonObject.getString(Constants.PARAM_OPEN_ID);
-            if (!TextUtils.isEmpty(token) && !TextUtils.isEmpty(expires) && !TextUtils.isEmpty(openId)) {
-                mTencent.setAccessToken(token, expires);
-                mTencent.setOpenId(openId);
-            }
-        } catch (Exception e) {
-        }
-    }
-
-    /**
-     * QQ登录第三步：获取用户信息
-     */
-    private void updateUserInfo() {
-        if (mTencent != null && mTencent.isSessionValid()) {
-            IUiListener listener = new IUiListener() {
-                @Override
-                public void onError(UiError e) {
-                    Message msg = new Message();
-                    msg.obj = "把手机时间改成获取网络时间";
-                    msg.what = 1;
-                    mHandler.sendMessage(msg);
-                }
-
-                @Override
-                public void onComplete(final Object response) {
-                    Message msg = new Message();
-                    msg.obj = response;
-                    msg.what = 0;
-                    mHandler.sendMessage(msg);
-                }
-
-                @Override
-                public void onCancel() {
-                    Message msg = new Message();
-                    msg.obj = "获取用户信息失败";
-                    msg.what = 2;
-                    mHandler.sendMessage(msg);
-                }
-            };
-            mInfo = new UserInfo(this, mTencent.getQQToken());
-            mInfo.getUserInfo(listener);
-        } else {
-            Toast.makeText(getApplicationContext(), "111", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            /** 获取用户信息成功 */
-            if (msg.what == 0) {
-                JSONObject response = (JSONObject) msg.obj;
-                if (response.has("nickname")) {
-                    Log.e("", "获取用户信息成功，返回结果：" + response.toString());
-                    try {
-                        tvReturnmessage.setText("登录成功\n" + "\n昵称:" + response.getString("nickname") + "\n头像地址:" + response.get("figureurl_qq_1"));
-                        x.image().bind(igUserAvatar, response.get("figureurl_qq_1").toString());
-                        Log.e("11", "22");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else if (msg.what == 1) {
-                tvReturnmessage.setText(msg + "");
-            } else if (msg.what == 2) {
-                tvReturnmessage.setText(msg + "");
-            }
-        }
-
-    };
-
-    /**
-     * QQ登录第一步：获取token和openid
-     */
-    private class BaseUiListener implements IUiListener {
-        @Override
-        public void onComplete(Object response) {
-            if (null == response) {
-                Toast.makeText(getApplicationContext(), "登录失败", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            JSONObject jsonResponse = (JSONObject) response;
-            if (null != jsonResponse && jsonResponse.length() == 0) {
-                Toast.makeText(getApplicationContext(), "登录失败", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            //Log.e("", "QQ登录成功返回结果-" + response.toString());
-            Toast.makeText(getApplicationContext(), "QQ登录成功返回结果", Toast.LENGTH_SHORT).show();
-            doComplete((JSONObject) response);
-        }
-
-        protected void doComplete(JSONObject response) {
-
-        }
-
-        @Override
-        public void onError(UiError e) {
-            //Util.toastMessage(MainActivity.this, "onError: " + e.errorDetail);
-            // Util.dismissDialog();
-            Toast.makeText(getApplicationContext(), "登录报错", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onCancel() {
-            Toast.makeText(MainActivity.this, "授权取消！", Toast.LENGTH_LONG).show();
-            if (isServerSideLogin) {
-                isServerSideLogin = false;
-            }
-        }
-    }
-
-    /**
-     * -------------------------QQ第三方登录结束--------------------
-     */
 
 
     //确保能接收到回调
@@ -222,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Tencent.onActivityResultData(requestCode, resultCode, data, loginListener);
+        Tencent.onActivityResultData(requestCode, resultCode, data, listener);
     }
 
 }
